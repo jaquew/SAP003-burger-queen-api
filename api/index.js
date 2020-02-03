d130ed553f0bb8c0270b0772d4590c7e383ac316
jaquew <eng.jaquesilva@gmail.com>
2020-02-03 11:41:33 -0300

    bug fix

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import productRoutes from './server/routes/ProductRoutes'
 import orderRoutes from './server/routes/OrderRoutes'
 import itemRoutes from './server/routes/ItemRoutes'
-import tableRoutes from './server/routes/TableRoutes'
+// import tableRoutes from './server/routes/TableRoutes'
 
 const app = express()
 app.use(bodyParser.json())
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000
 app.use('/orders', orderRoutes)
 app.use('/products', productRoutes)
 app.use('/items', itemRoutes)
-app.use('/tables', tableRoutes)
+// app.use('/tables', tableRoutes)
 
 
 
